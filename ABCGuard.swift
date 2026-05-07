6e2835916ec3e21f9d4317327638e872af71b911
ABC <782624687@qq.com>
2026-05-07 20:08:48 +0800

    Update ABCGuard.swift

--- ABCGuard.swift
+++ ABCGuard.swift
@@ -1,27 +1,22 @@
 import Foundation
 import UIKit
-import WebKit
 
 @_cdecl("initialize")
 public func initialize() {
-    print("ABCGuard: Dynamic library loaded.")
+    print("ABCGuard loaded")
 }
 
 @_cdecl("startGuard")
 public func startGuard() {
-    print("ABCGuard: startGuard() called.")
+    print("startGuard called")
 }
 
 private let _init: Void = {
-    print("ABCGuard: Static initializer executed.")
+    print("Static init")
 }()
 
-public class ABCGuardManager {
-    public static let shared = ABCGuardManager()
-    private init() {
-        print("ABCGuardManager initialized")
-    }
-    public func run() {
-        print("ABCGuardManager run")
-    }
+public class Manager {
+    public static let shared = Manager()
+    private init() {}
+    public func run() { print("run") }
 }
